a55b8de809509ea25b755abd62bde4c616ba9061
Issac <onokokono@gmail.com>
2020-06-26 17:20:20 -0700

    Updates

---
+++ precache-manifest.85d2574ad82c95b26fbf7c3a3fbe621e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48016c65250c3b842d51e405ec0d95f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9b169e829efc59d6924",
+    "url": "/static/css/main.dadebc63.chunk.css"
+  },
+  {
+    "revision": "2613c05d28527c200192",
+    "url": "/static/js/2.85f812ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.85f812ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9b169e829efc59d6924",
+    "url": "/static/js/main.1158110c.chunk.js"
+  },
+  {
+    "revision": "9ba694429dc9dc708504",
+    "url": "/static/js/runtime-main.1e28496c.js"
+  },
+  {
+    "revision": "8ea2657f263b6b2a404bfb57f4160013",
+    "url": "/static/media/Poppins-ExtraLight.8ea2657f.ttf"
+  },
+  {
+    "revision": "2a47a29ceb33c966c8d79f8d5a5ea448",
+    "url": "/static/media/Poppins-Light.2a47a29c.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/static/media/Poppins-Regular.41e8dead.ttf"
+  }
+]);
